7cb950083a131a6d248c16653fcb9b3334c39be3
calvin.davies <calvin.davies@benevolent.ai>
2022-07-07 09:51:09 +0100

    chore(utest): Increasing Unit test coverage.

--- tests/test_node.cpp
+++ tests/test_node.cpp
@@ -1,7 +1,25 @@
 #include <catch2/catch_test_macros.hpp>
 #include <lib/node.cpp>
 
-TEST_CASE( "Strings exists in TernaryTree", "[node]" ) {
+TEST_CASE( "Test insertNode function with individual strings.", "[node]" ) {
+    Node* root = nullptr;
+
+    SECTION ( "Testing with valid words." ) {
+        root = insertNode(root, "cat", 0);
+        root = insertNode(root, "cation", 0);
+        REQUIRE( searchTST(root, "cat") == true );
+        REQUIRE( searchTST(root, "cation") == true );
+    }
+
+    SECTION ( "Testing with valid chars." ) {
+        root = insertNode(root, "1234", 0);
+        root = insertNode(root, "!@£$", 0);
+        REQUIRE( searchTST(root, "1234") == true );
+        REQUIRE( searchTST(root, "!@£$") == true );
+    }
+}
+
+TEST_CASE( "Test buildTST function with Vector of strings", "[node]" ) {
     std::vector<std::string> treeStrings {"cat", "catch", "cation", "dog"};
 
     Node* root = buildTST(treeStrings);
